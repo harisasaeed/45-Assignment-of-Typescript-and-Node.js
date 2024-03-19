be081b9c6b4b29b43c95c2410f2b2bd28dcbab01
Harisa Saeed <155890055+harisasaeed@users.noreply.github.com>
2024-03-19 09:37:44 -0700

    Add files via upload

---
+++ task10.js
@@ -0,0 +1,6 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//author: [harisa saeed]
+//date: [monday, feb 12,2024]
+//This program print a  personal message.
+var myName = "harisa";
+console.log("Hello  ".concat(myName, ", would you like to learn some Typescript tody?"));
---
+++ task8.js
@@ -0,0 +1,7 @@
+//you should creat 8 lines that look like this:
+//console.log(5 + 3);
+//Task 8
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
